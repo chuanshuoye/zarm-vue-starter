f259145af68a22cb9f0457691ae33f825333e27c
叶展旺 <yezhanwang@yezhanwangdeMacBook-Pro.local>
2018-04-23 17:31:32 +0800

    Init starter

---
+++ src/app.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style>
+  .demo{
+    width:325px;height:50px;border:1px solid #333;
+  }
+</style>
---
+++ src/components/Page.vue
@@ -0,0 +1,22 @@
+<template>
+ <div>
+  <za-button :theme='buttonTheme' size='lg' shape='round'>{{name}}</za-button>
+ </div>
+</template>
+<script lang="ts">
+ import Vue from 'vue'
+
+ const getName = (name: string | null) => {
+   if (name === null) return '12';
+   if (name.length) return name;
+ }
+ 
+  export default Vue.extend({
+  data() {
+    return {
+      name: '',
+      buttonTheme: '123'
+    }
+  }
+ })
+</script>
