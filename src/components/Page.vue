5a123986dc54cd3028117eb52c043c582826018e
yezhanwang <232465450@qq.com>
2019-03-20 23:20:45 +0800

    updated to 2.0.0-alpha

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -1,73 +1,21 @@
 <template>
   <div>
-    <za-notice-bar hasArrow>带图标，带右侧箭头的链接样式</za-notice-bar>
-    <za-tabs>
-      <za-tab-pane label='用户管理' name='first'>
-        <div class='content'>用户管理</div>
-      </za-tab-pane>
-      <za-tab-pane label='配置管理' name='second'>
-        <div class='content'>配置管理</div>
-      </za-tab-pane>
-      <za-tab-pane label='角色管理' name='third'>
-        <div class='content'>角色管理</div>
-      </za-tab-pane>
-    </za-tabs>
-    
-    
-    <za-cell hasArrow title="点状">
-      <za-badge slot='description' shape='dot'></za-badge>
-    </za-cell>
-    <za-cell title='标题文字'></za-cell>
     <za-cell>
-      <za-checkbox-group>
-        <za-checkbox>普通</za-checkbox>
-      </za-checkbox-group>
-    </za-cell>
-    <za-cell>
-      <za-radio-group>
-        <za-radio>普通</za-radio>
-      </za-radio-group>
-    </za-cell>
-    <za-cell title='单行文本'>
-      <za-input ref='inputFirst' type="text" placeholder="type is text"></za-input>
-    </za-cell>
-    <za-message>primary</za-message>
-    <zaAccordion>
-      <zaAccordionItem title="50元套餐" aiTag='0'>
-        <div>我是50元套餐内容</div>
-        <div>我是50元套餐内容</div>
-        <div>我是50元套餐内容</div>
-        <div>我是50元套餐内容</div>
-      </zaAccordionItem>
-    </zaAccordion>
-    <za-panel>
-      <za-panel-header title='普通' />
-      <za-panel-body>
-        <za-cell>body</za-cell>
-      </za-panel-body>
-    </za-panel>
-    <za-cell>
-      <za-spinner slot='description' class="rotate360" /> 普通
-    </za-cell>
-    <za-cell title="普通">
-      <za-stepper slot='description'></za-stepper>
-    </za-cell>
-    <za-cell>
-      <za-switch slot='description'></za-switch>
-      普通
-    </za-cell>
-    
-    <div>
       <za-button theme="primary" block>普通按钮</za-button>
-    </div>
+    </za-cell>
   </div>
 </template>
 <script>
-  export default {
-    data() {
-      return {
-        name: '123'
-      }
+import { Cell, Button } from "zarm-vue";
+export default {
+  components: {
+    zaCell: Cell,
+    zaButton: Button
+  },
+  data() {
+    return {
+      name: '123'
     }
   }
+}
 </script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
-import 'zarm-vue/zarm-vue.default.css'
 import Vue from 'vue'
-import ZarmVue from 'zarm-vue';
 import App from './app.vue'
 import router from './router'
 
 Vue.config.productionTip = false
-Vue.use(ZarmVue);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
