19f55717ecbaaaa247bb22d672b8e39dff2a6a0e
zhanwang.ye <232465450@qq.com>
2019-11-13 16:14:42 +0800

    alpha34

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import ZarmVue from 'zarm-vue'
 import App from './app.vue'
 import router from './router'
 
+import 'zarm-vue/zarm-vue.default.css'
+
+Vue.use(ZarmVue)
 Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
